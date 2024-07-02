462358737356a073d6884c3f0a03fa8fdf95057e
hiroki <hir_koba.1389@outlook.com>
2024-07-02 12:06:18 +0900

    Initial Commit

---
+++ double_bookkeeping/double_bookkeepingApp.swift
@@ -0,0 +1,17 @@
+//
+//  double_bookkeepingApp.swift
+//  double_bookkeeping
+//
+//  Created by Hiroki Kobayashi on 2024/07/02.
+//
+
+import SwiftUI
+
+@main
+struct double_bookkeepingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
